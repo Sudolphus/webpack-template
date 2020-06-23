bc9bc94d01b765e88e20509149097d7095254852
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-22 17:11:48 -0700

    update template with jquery and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-import './bootstrap.css';
+import $ from 'jQuery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { pingPong } from './ping-pong';
-
-$(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    const goal = $("#goal").val();
-    const output = pingPong(goal);
-    output.forEach(function(element) {
-      $("#solution").append(`<li>${element}</li>`);
-    });
-  });
-});
